a1fce26c7514193e29b1174a8ace67c000f0ae04
NIDHISH99444 <38004278+NIDHISH99444@users.noreply.github.com>
2018-04-03 10:47:57 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -14,23 +14,23 @@ avatar = media.Movie("Avatar",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
 matrix = media.Movie("Matrix",
-                     "A movie about the reality of life itself.",
+                     "Movie portraying the reality of life",
                      "http://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
                      "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
 
 whiplash = media.Movie("Whiplash",
-                       "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.",
+                       "A dedicated young man ",
                        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSyLORvKKvCi7-vy8vwi2s8F62aG7D36H15A8rOVfP2d7koyA9I",
                        "https://www.youtube.com/watch?v=tYkuvB2f5XU")
 
 mad_max_fury_road = media.Movie("Mad Max Fury Road",
-                                "In a stark desert landscape where humanity is broken, two rebels just might be able to restore order: Max, a man of action and of few words, and Furiosa, a woman of action who is looking to make it back to her childhood homeland.",
+                                "Classic Graphic Effects movie",
                                 "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSY9szIPbtk1-hwxdEVRJIHT_pgYGNnFkFSWsCjlKFGP3Pu77Oo",
                                 "https://www.youtube.com/watch?v=YWNWi-ZWL3c")
 
 above_the_rim = media.Movie("Above the Rim",
-                            "A promising New York City high school basketball star and his relationships with two people.",
+                            "Basketball Stars and there relationships",
                             "http://upload.wikimedia.org/wikipedia/en/b/b6/Above_the_rim_poster.jpg",
                             "https://www.youtube.com/watch?v=sEsCXWD7-Cc")
 
